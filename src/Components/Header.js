d3c806a73e832cec0e8b8e21f1fe7e503f849ba8
Thanwar <thawardas25@gmail.com>
2021-01-16 02:42:29 +0500

    function fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import "./header.css"
 export default function Header() {
   return (
     <div className="header">
-      <h1>Virtual lollipop !</h1>
+      <h1>Virtual lollipop</h1>
       <p>because we all know someone <br /> who deserves some sugar.</p>
     </div>
   )
